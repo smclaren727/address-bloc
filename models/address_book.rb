805c9172e825104618444a439b21ef051037b443
smclaren727 <smclaren727@gmail.com>
2015-06-09 14:51:11 -0400

    Checkpoint 23 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
 #7 - Load the library named entry.rb
 require_relative "entry.rb"
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -20,5 +21,22 @@ class AddressBook
     end
     #10 - Insert a new entry into entries using the calculated index from steps 8 & 9.
     @entries.insert(index, Entry.new(name, phone, email))
+  end
+
+  # Defined the method. Starts by reading the file using File.read and uses the CSV
+  # class to parse the file
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true)
+    # Iterate over the table object's rows
+    csv.each do |row|
+      # Create a hash for each row
+      row_hash = row.to_hash
+      # Convert each row_hash to an Entry by using add_entry method
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+
+    # Get the number of items parsed by calling the count method
+    return csv.count
   end
 end
